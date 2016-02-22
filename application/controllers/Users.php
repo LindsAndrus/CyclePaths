0c064d1c109446432074ca8bd949b4398200ae82
Steven <nguyenstevenn@gmail.com>
2016-02-22 12:28:43 -0800

    added css and log/reg page

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,8 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Users extends CI_Controller {
 
-	public function index()
-	{
-		$this->load->view('dashboard');
+	public function signin() {
+
 	}
+
+	public function create() {
+
+	}
+
+	public function login_reg_page(){
+		$this->load->view('users/login_reg_page');
+	}
+
+
 }
