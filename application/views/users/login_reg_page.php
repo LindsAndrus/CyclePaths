9c85d03feee2e355f1bec01f45c6862e7358fc3d
Steven <nguyenstevenn@gmail.com>
2016-02-22 13:02:47 -0800

    Updated the header partial and the custom.css

--- application/views/users/login_reg_page.php
+++ application/views/users/login_reg_page.php
@@ -3,7 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Login and Registration</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<link rel="stylesheet" href="/assets/css/custom.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 </head>
 <body>
 	<?php $this->load->view('header') ?>
