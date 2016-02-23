c42bf6d6761ba752d70325c94c4eacd491176525
Nick Collins <nicksc423@gmail.com>
2016-02-22 19:52:49 -0800

    Updated the user edit/update functions to limit to your own user

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -56,43 +56,48 @@ class Users extends CI_Controller {
 
 	}
 	public function edit($id){
-		$this->load->model('User');
-		$user = $this->User->get_user_by_id($id);
-		$user['id'] = $id;
-		$this->load->view('users/edit', array('user' => $user));
+		if($id != $this->session->userdata('user_id')) {
+			show_404();
+		} else {
+			$this->load->model('User');
+			$user = $this->User->get_user_by_id($id);
+			$user['id'] = $id;
+			$this->load->view('users/edit', array('user' => $user));
+		}
 	}
 
 	public function update() {
-		$this->load->model('User');
-		$user = $this->User->get_user_by_id($this->session->userdata('user_id'));
 		$data = $this->input->post();
-		if(!$this->User->update_validate($data)) {
-			//set errors
-			$errors = array(validation_errors()); 
-			$this->session->set_flashdata('edit_errors', $errors);
-			redirect("/users/edit/".$this->session->userdata('user_id'));
+		if($data['id'] != $this->session->userdata('user_id')) {
+			show_404();
 		} else {
-			//edit the user
-			if(password_verify($data['password'], $user['password'])) {
-				$data += array('user_id'=>$this->session->userdata('user_id'));
-				$this->User->update_user($data);
-				#redirect to home page
-				redirect("/users/show/".$this->session->userdata('user_id')); 
-			} else {
+			$this->load->model('User');
+			$user = $this->User->get_user_by_id($this->session->userdata('user_id'));
+			if (!$this->User->update_validate($data)) {
 				//set errors
-				$this->session->set_flashdata('edit_errors', "Invalid password!");
-				redirect("/users/edit/".$this->session->userdata('user_id')) ;
+				$errors = array(validation_errors());
+				$this->session->set_flashdata('edit_errors', $errors);
+				redirect("/users/edit/" . $this->session->userdata('user_id'));
+			} else {
+				//edit the user
+				if (password_verify($data['password'], $user['password'])) {
+					$data += array('user_id' => $this->session->userdata('user_id'));
+					$this->User->update_user($data);
+					#redirect to home page
+					redirect("/users/show/" . $this->session->userdata('user_id'));
+				} else {
+					//set errors
+					$this->session->set_flashdata('edit_errors', "Invalid password!");
+					redirect("/users/edit/" . $this->session->userdata('user_id'));
+				}
 			}
 		}
 	}
-		public function logout()
-  		{
-	    
-	    $this->session->sess_destroy();
-	    redirect("/");
-		}
 
-
+	public function logout() {
+		$this->session->sess_destroy();
+		redirect("/");
+	}
 
 	public function login_reg_page(){
 		$this->load->view('users/login_reg_page');
