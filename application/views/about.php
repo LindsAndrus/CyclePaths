872506fe06784eb40679046b0821b4802974bf9e
Ethan Choi <dchoi33@yahoo.com>
2016-02-24 09:28:48 -0800

    styling updates

--- application/views/about.php
+++ application/views/about.php
@@ -23,19 +23,19 @@
 	<?php $this->load->view('header') ?>
 
 	<div id="about_container">
-		
-		<div class="about_text">
-			<h2 id="about_header">About CyclePaths</h2>
-			<p>On a cold and dreary winter Seattle day, four students came together with a mutual dream of creating the best bike-parts sales platform ever. The four 'Cyclepaths' thought a simple easy to use directory of used parts would help budget savy bikers find the parts to bring their hogs to life. </p>
+		<div id="content_container">
+			<div class="about_text">
+				<h2 id="about_header">About CyclePaths</h2>
+				<p>On a cold and dreary winter Seattle day, four students came together with a mutual dream of creating the best bike-parts sales platform ever. The four 'Cyclepaths' thought a simple easy to use directory of used parts would help budget savy bikers find the parts to bring their hogs to life. </p>
+			</div>
+			
+			<div class="portrait">
+				<img src="assets/images/portrait.png" class="about_portrait" id="nick">
+				<img src="assets/images/portrait.png" class="about_portrait" id="steven"><br>
+				<img src="assets/images/portrait.png" class="about_portrait" id="lindsey">
+				<img src="assets/images/portrait.png" class="about_portrait" id="ethan">
+			</div>
 		</div>
-		
-		<div class="portrait">
-			<img src="assets/images/portrait.png" class="about_portrait" id="nick">
-			<img src="assets/images/portrait.png" class="about_portrait" id="steven"><br>
-			<img src="assets/images/portrait.png" class="about_portrait" id="lindsey">
-			<img src="assets/images/portrait.png" class="about_portrait" id="ethan">
-		</div>
-
 		<!-- If time allows, incorporate hover function to display team member bio -->
 
 	</div>
--- application/views/header.php
+++ application/views/header.php
@@ -1,29 +1,34 @@
 <style type="text/css">
 	.logo{
 		display: inline-block;
-		margin-right: 300px;
+		margin-right: 50px;
 	}
 
 	.links{
 		display: inline-block;
 	}
 	.container{
-		margin-top: 10px;
-		width: 900px;
+		padding-top: 10px;
+		margin: auto;
 	}
 </style>
 
 <div class="container">
-	<div class="logo">
+
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="logo">
 		<img src="/assets/images/logo.png" height="60px" width="250px"></img>
-	</div>
-	<div class="links">
-		<?php if($this->session->userdata('user_id')) : ?>
-			<a href="/logout">LOGOUT</a>
-		 <?php else : ?>
-		 	<a href="/login">LOGIN/REGISTRATION</a>
-		 <?php endif ?>
-		<a href="/about">ABOUT</a>
-		<a href="/contact">CONTACT US</a>
-	</div>
+		</div>
+
+	  	<div class="container">
+	    	<?php if($this->session->userdata('user_id')) : ?>
+				<a href="/logout">LOGOUT</a>
+		 	<?php else : ?>
+		 		<a href="/login">LOGIN/REGISTRATION</a>
+		 	<?php endif ?>
+			<a href="/about">ABOUT</a>
+			<a href="/contact">CONTACT US</a>
+	  	</div>
+	</nav>
+
 </div>
