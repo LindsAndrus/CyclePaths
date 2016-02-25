97e86946c171b0a552ba56dfbafbf04297d21856
LindsAndrus <LindsAndrus@hotmail.com>
2016-02-24 16:52:02 -0800

    updated formatting login/reg

--- application/views/users/login_reg_page.php
+++ application/views/users/login_reg_page.php
@@ -8,17 +8,6 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	 <link rel="stylesheet" href="/assets/css/header.css">
 
-	<style type="text/css">
-  	#main_container{
-		background: url("assets/images/bike.png");
-		background-repeat:no-repeat;
-		-webkit-background-size:cover;
-		-moz-background-size:cover;
-		-o-background-size:cover;
-		background-size:cover;
-		background-position:center;
-	}
-	</style>
 </head>
 <body>
 	<?php $this->load->view('header') ?>
