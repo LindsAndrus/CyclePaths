ef34d6f6f4c3335f1854103a01087d8e95b0364a
Steven <nguyenstevenn@gmail.com>
2016-02-25 10:54:05 -0800

    fixed header and added new link

--- application/views/header.php
+++ application/views/header.php
@@ -9,6 +9,7 @@
 	 	<?php else : ?>
 	 		<li><a href="/login">LOGIN/REGISTRATION</a></li>
 	 	<?php endif ?>
+	 	<li><a href="/listings/create_item">CREATE LISTING</a></li>
 		<li><a href="/about">ABOUT</a></li>
 		<li><a href="/contact">CONTACT US</a></li>
 	</ul>
