44730f3e5c3e821ad6e1c45d9452bfddae19dc20
Ethan Choi <dchoi33@yahoo.com>
2016-02-25 12:45:27 -0800

    styling changes

--- application/views/users/edit.php
+++ application/views/users/edit.php
@@ -2,20 +2,21 @@
 <html lang="en">
 <head>
   	<meta charset="UTF-8">
-	<title>Seller Page</title>
+	<title>Edit Your Profile</title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-	<link rel="stylesheet" href="/assets/css/custom.css">
-	<link rel="stylesheet" href="/assets/css/header.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<link rel="stylesheet" href="/assets/css/header.css">
+	<link rel="stylesheet" href="/assets/css/custom.css">
+	<link rel="stylesheet" href="/assets/css/style.css">
+
+	
 </head>
 <body>
 
-  <div class="header">
-    <?php $this->load->view('header'); ?>
-  </div>
+<?php $this->load->view('header'); ?>
 
-<div class="container">
-	<div class="edit">	
+<div class="profile_container" id="main_container">
+	<div id="profile_edit">	
 	    <h3>Edit Here!</h3>
     	<?php if($this->session->flashdata('edit_errors')) : ?>
     		<div class="error">
@@ -26,14 +27,33 @@
 		<?php endif?>
 		<?php echo form_open_multipart('users/update');?>
 			<h4>Profile Picture</h4><input type="file" name="userfile"/>
-			<h4>First Name</h4><input value="<?=$user['first_name']?>" type="text" name="first_name">
-			<h4>Last Name</h4><input value="<?=$user['last_name']?>" type="text" name="last_name">
-			<h4>Email</h4><input value="<?=$user['email']?>" type="text" name="email">
-			<h4>Password</h4><input type="password" name="password">
+			
+			<div class="input-group">
+				<input type="text" name="first_name" class="form-control" value="<?=$user['first_name']?>" aria-describedby="basic-addon1">
+			</div>
+			<div class="input-group">
+			  	<input type="text" name="first_name" class="form-control" value="<?=$user['last_name']?>" aria-describedby="basic-addon1">
+			</div>
+			<div class="input-group">
+			  	<input type="text" name="first_name" class="form-control" value="<?=$user['email']?>" aria-describedby="basic-addon1">
+			</div>
+			<div class="input-group">
+			  	<input type="password" name="password" class="form-control" placeholder="Password" aria-describedby="basic-addon1">
+			</div>
 			<input type="hidden" name="id" value="<?= $user['user_id']?>">
-			<br>
 			<input type="submit" value="Edit Account" class="btn btn-primary">
+			<p>(Enter your password to save changes)</p>
 		</form>
+
+
+			<!-- <h4>First Name</h4><input value="<?=$user['first_name']?>" type="text" name="first_name"> -->
+			<!-- <h4>Last Name</h4><input value="<?=$user['last_name']?>" type="text" name="last_name"> -->
+			<!-- <h4>Email</h4><input value="<?=$user['email']?>" type="text" name="email"> -->
+			<!-- <h4>Password</h4><input type="password" name="password"> -->
+			<!-- <input type="hidden" name="id" value="<?= $user['user_id']?>">
+			<br>
+			<input type="submit" value="Edit Account" class="btn btn-primary"> -->
+		
 	</div>
 </div>
 
